5a48fe3451f4715e586e5ab57f877c8f7d82b85a
esinyakov <43815165+esinyakov@users.noreply.github.com>
2019-04-03 22:52:18 +0300

    Update Lab.c

--- Lab6/Task1/Lab.c
+++ Lab6/Task1/Lab.c
@@ -6,25 +6,25 @@
 int main()
 {
 
-    int const n =4 , m =5;
+	int const n =4 , m =5;
 	int a[n][m],b[n];	
-    int i,j,min,tmp,k,r = 100;
+	int i,j,min,tmp,k,r = 100;
 
-    srand((unsigned)time(NULL));
+	srand((unsigned)time(NULL));
 
-    printf("\nArray:\n");
-	
-    for (i = 0; i < n; i++)
-    {
+	printf("\nArray:\n");
+
+	for (i = 0; i < n; i++)
+	{
 		for (j = 0; j < m; j++)
 		{
 			a[i][j] = (int)(rand()%r);
 		}
-        
-    }
-	
+
+	}
+
 	for (i = 0; i < n; i++)
-    {
+	{
 		min = a[i][0];
 		for (j = 0; j < m; j++)
 		{
@@ -33,46 +33,46 @@ int main()
 		}
 		printf(" min:%d\n",min);
 		b[i] = min;       
-    }
-    
+	}
+
 
 	for(i = 0 ; i < n - 1; i++) 
 	{ 
-       for(j = 0; j < n - 1 - i ; j++) 
-	   {  
-           if(b[j] > b[j+1]) 
-		   {           
+		for(j = 0; j < n - 1 - i ; j++) 
+	   	{  
+	   		if(b[j] > b[j+1]) 
+			{           
 				tmp = b[j+1];
 				b[j+1] = b[j];
 				b[j]=tmp;
-				
+
 				for(k=0; k < m; k++)
 				{	
 					tmp = a[j+1][k];
 					a[j+1][k]=a[j][k];
 					a[j][k]=tmp;
 				}
-           }
-        }
-    }
-    
-    printf("\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ",b[i]);
-    }
-    
-    printf("\n\n");
-    
+	   		}
+		}
+	}
+
+	printf("\n");
 	for (i = 0; i < n; i++)
-    {
+	{
+		printf("%d ",b[i]);
+	}
+
+	printf("\n\n");
+
+	for (i = 0; i < n; i++)
+	{
 		for (j = 0; j < m; j++)
 		{
 			printf("%d ",a[i][j]);
 		}
 		printf("\n");     
-    }
-	
+	}
+
 	getch();
 	return 0;
 }
