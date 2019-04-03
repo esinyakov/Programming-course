f6e1dd37e6e3113b9c4566b89cd65910a612986f
esinyakov <43815165+esinyakov@users.noreply.github.com>
2019-04-03 22:53:47 +0300

    Update Lab.c

--- Lab6/Task2/Lab.c
+++ Lab6/Task2/Lab.c
@@ -6,58 +6,58 @@
 int main()
 {
 
-    const int n =4;
+	const int n =4;
 	int a[n][n];
-    int i,j,tmp,r = 100;
+	int i,j,tmp,r = 100;
 	int* pa;
-    srand((unsigned)time(NULL));
+	srand((unsigned)time(NULL));
 
 	pa = a;
-	
-    printf("\nArray:\n");
-	
-    for (i = 0; i < n; i++)
-    {
+
+	printf("\nArray:\n");
+
+	for (i = 0; i < n; i++)
+	{
 		for (j = 0; j < n; j++)
 		{
 			a[i][j] = (int)(rand()%r);
 		}
-        
-    }
-	
+
+	}
+
 	for (i = 0; i < n; i++)
-    {
+	{
 		for (j = 0; j < n; j++)
 		{
 			printf("%d ",a[i][j]);
 		}
 		printf("\n");
-    }
-    
+	}
+
 
 	for(i = 0 ; i < n*n - 1; i++) 
 	{ 
-       for(j = 0; j < n*n - 1 - i ; j++) 
-	   {  
-           if(*(pa+j) > *(pa+j+1)) 
-		   {           
+		for(j = 0; j < n*n - 1 - i ; j++) 
+	   	{  
+	   		if(*(pa+j) > *(pa+j+1)) 
+		   	{           
 				tmp = *(pa+j+1);
 				*(pa+j+1) = *(pa+j);
 				*(pa+j)=tmp;
-           }
-        }
-    }
-    
-    printf("\n\n");
-    for (i = 0; i < n; i++)
-    {
+	   		}
+		}
+	}
+
+	printf("\n\n");
+	for (i = 0; i < n; i++)
+	{
 		for (j = 0; j < n; j++)
 		{
 			printf("%d ",a[i][j]);
 		}
 		printf("\n");
-    }
-    
+	}
+
 	for(i=0;i<n;i++)
 	{
 		for(j=i;j<n;j++)
@@ -67,18 +67,18 @@ int main()
 			a[j][i]=tmp;
 		}
 	}
-    
-    printf("\n\n");
-    
+
+	printf("\n\n");
+
 	for (i = 0; i < n; i++)
-    {
+	{
 		for (j = 0; j < n; j++)
 		{
 			printf("%d ",a[i][j]);
 		}
 		printf("\n");     
-    }
-	
+	}
+
 	getch();
 	return 0;
-}
+	}
